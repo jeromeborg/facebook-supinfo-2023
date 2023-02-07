36e66969f2922f62ecc6776b5eccc2e2ee2fc061
jeromeborg <contact@jeromeb.org>
2023-02-07 09:08:18 +0100

    feat(front) promotion and user migrations, factories, seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->lastName(),
+            'firstname' => fake()->firstName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'promotion_id' => rand(1,2),
+            'avatar' => fake()->imageUrl($width = 120, $height = 240)
         ];
     }
 
